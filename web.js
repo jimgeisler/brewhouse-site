a695590eddba8e72da066afb8e1a3a39fd81803f
Jim Geisler <jim.geisler@gmail.com>
2012-11-02 22:34:36 -0400

    fixed port bug

--- web.js
+++ web.js
@@ -4,6 +4,8 @@ var app = express.createServer();
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res){res.render('index.jade', {title: 'Franz Enzenhofer'});});
+app.get('/', function(req, res){res.render('index.jade');});
 
-app.listen(5000);
+var port = process.env.PORT || 5000;
+
+app.listen(port);

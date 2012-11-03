ba86491b28ba1e6da0eda78df03a3517086afe63
Jim Geisler <jim.geisler@gmail.com>
2012-11-03 09:34:02 -0400

    Recipes are read in from JSON file.

--- web.js
+++ web.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express.createServer();
+var fs = require('fs');
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res){res.render('index.jade');});
+
+app.get('/recipes', function(req, res){
+    var file = './data/recipes.json';
+
+    fs.readFile(file, 'utf8', function (err, data) {
+      if (err) {
+          console.log(err)
+      }
+
+      res.render('recipes.jade', JSON.parse(data));
+    });
+});
+
 
 var port = process.env.PORT || 5000;
 

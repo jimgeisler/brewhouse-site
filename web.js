799b5b3a4556c0016f1c9369c6819e3578affb66
Jim Geisler <jim.geisler@gmail.com>
2012-11-02 22:26:48 -0400

    Switched to jade

--- web.js
+++ web.js
@@ -1,16 +1,9 @@
 var express = require('express');
+var app = express.createServer();
 
-var app = express.createServer(express.logger());
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname));
 
-app.get('/', function(request, res) {
-  res.sendfile(__dirname + "/html/index.html");
-});
+app.get('/', function(req, res){res.render('index.jade', {title: 'Franz Enzenhofer'});});
 
-app.get('*.js|*.css|*.jpg|*.gif|*.png|*.ttf', function (req,res){
-  res.sendfile(__dirname + req.url);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(5000);
